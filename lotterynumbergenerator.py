ab6814423538f07984d06f612af1357493e86a9c
yasirkevin <89997043+yasirkevin@users.noreply.github.com>
2021-09-02 23:09:36 +0100

    Update lotterynumbergenerator.py

--- lotterynumbergenerator.py
+++ lotterynumbergenerator.py
@@ -1,1 +1,44 @@
 
+import random
+
+RANGE = 49
+GENERATIONS = 6
+
+numbers = []
+inputs = []
+numbersIncorrect = GENERATIONS
+
+def check_repeated(num):
+  for i in range(1, len(numbers)):
+    if numbers[i] == num:
+      return True
+  return False
+
+for i in range(1, GENERATIONS + 1):
+  generatedNumber = random.randint(1, 49)
+  userInput = 0
+  metConditions = False
+  while not(metConditions):
+    userInput = ""
+
+    if check_repeated(generatedNumber) == False:
+      try:
+          userInput = int(input("[{}] Guess the number: ".format(i)))
+          metConditions = True
+      except ValueError:
+          print("[!] Invalid number")
+    else:
+      generatedNumber = random.randint(1, 49)
+  
+
+  numbers.append(generatedNumber)
+  inputs.append(userInput)
+
+print("\n The numbers were: \n")
+
+for i in range(0, GENERATIONS):
+  if numbers[i] == inputs[i]:
+    numbersIncorrect -= 1
+    print("[{}] Correct!".format(i + 1))
+  else:
+    print("[{}] Incorrect! The number was: {}".format(i + 1, numbers[i]))
