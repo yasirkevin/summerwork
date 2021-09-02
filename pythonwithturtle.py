25834c4403d05500e718425887aedad3a8f110d3
yasirkevin <89997043+yasirkevin@users.noreply.github.com>
2021-09-03 00:23:11 +0100

    Create pythonwithturtle.py

---
+++ pythonwithturtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+bob = turtle.Turtle()
+
+for i in range(11):
+  bob.forward(50)
+  bob.right(50)
+  bob.forward(50)
+  bob.right(130)
+  bob.forward(50)
+  bob.right(50)
+  bob.forward(50)
+
