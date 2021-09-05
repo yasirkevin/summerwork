7dea5d5fd066a5aebc904f4b9f16bf095e6acbaa
yasirkevin <89997043+yasirkevin@users.noreply.github.com>
2021-09-06 00:51:51 +0100

    Update wordopposites.py

--- wordopposites.py
+++ wordopposites.py
@@ -1,30 +1,92 @@
 import random
 
-# rushed
+QUESTIONS = 10
 
-words = "hot, summer, hard, dry, simple, light, weak, male, sad, win, small, ignore, buy, succeed, reject, prevent, exclude, top, useless, stand".split(", ")
-opposites = "cold, winter, soft, wet, complex, darkness, strong, female, happy, lose, big, pay attention, sell, fail, accept, allow, include, bottom, useful, sit".split(",")
-
-wordsNumber = len(words)
+invalidCharacters = ",[]_-\\"
+data = [
+  # [[KEY], [SYNONYMS], [ANTONYMS]]
+]
 score = 0
 
-for i in range(1, 11):
-  random1 = random.randint(0, len(words) - 1)
-  
-  print("Q{}: {} is to {}".format(i, words[random1], opposites[random1]))
-  words.remove(words[random1])
-  opposites.remove(opposites[random1])
+with open('data_oppositewords.txt') as f:
+    allLines = f.readlines()
 
-  random2 = random.randint(0, len(opposites) - 1)
-  userInput = input("as {} is to ".format(opposites[random2]))
-  
-  if userInput.upper() == words[random2].upper():
-      score += 1
-      
-  words.remove(words[random2])
-  opposites.remove(opposites[random2])
+    for line in range(1, len(allLines)):
+        content = allLines[line]
 
-print(score)
-      
+        if ("KEY:" in content):
+          wordEnd = content.find(".")
+          word = content[5: wordEnd]
+          foundInvalid = False
+
+          for i in range(len(invalidCharacters)):
+            if invalidCharacters[i] in word or len(word) <= 2:
+                foundInvalid = True
+
+          if foundInvalid:
+            continue
+
+          data.append([[word], [], []])      
+
+        elif ("SYN:" in content):
+          words = content[5: content.find(".") or len(content)].split(", ")
+          
+          for word in words:
+            foundInvalid = False
+            if foundInvalid:
+              continue
+
+            for char in invalidCharacters:
+              if char in word:
+                foundInvalid = True
+                words.remove(word)
+                break
+
+          data[len(data) - 1][1] = words
+
+        elif ("ANT:" in content):
+          words = content[5: content.find(".") or len(content)].split(", ")
+          
+          for word in words:
+            foundInvalid = False
+            if foundInvalid:
+              continue
+
+            for char in invalidCharacters:
+              if char in word:
+                foundInvalid = True
+                words.remove(word)
+                break
+        
+
+          data[len(data) - 1][2] = words
+
+    
+
+for questionNumber in range(QUESTIONS):
+  while True:
+    firstRandomWord =  data[random.randint(0, len(data) - 1)]
+
+    if len(firstRandomWord[2]) > 2:
+      firstRandomAnt = firstRandomWord[2][random.randint(0, len(firstRandomWord[2]) - 1)]
+      break
+
+  while True:
+    secondRandomWord = data[random.randint(0, len(data) - 1)]
+    #print(secondRandomWord[0][0], firstRandomWord[0][0])
+    if secondRandomWord != firstRandomWord:
+      break
+  
+  answer = input("Q{}: {} is to {}, as {} is to "
+  .format(questionNumber + 1, firstRandomWord[0][0].upper(), firstRandomAnt.upper(), secondRandomWord[0][0].upper()))
+
+  if answer.upper() in secondRandomWord[2]:
+    score += 1
+
+print("Score: {}".format(score))
+
+
+
+  
 
 
